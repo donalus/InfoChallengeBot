51ccb10b559b29933b7ab7d841896fd4e3c0eb09
Donal Heidenblad <dheidenb@umd.edu>
2022-01-13 09:13:00 -0500

    Connect to mariadb (#9)
    * remove debug from dockerfile
    
    * Fixed issues with sqlalchemy to enable connection to mariadb

--- src/models/convostate.py
+++ src/models/convostate.py
@@ -1,16 +1,16 @@
 from sqlalchemy import Column, Integer, String
-from . import Base
+from . import Base, UnsignedInt
 
 
 class ConvoState(Base):
     __tablename__ = 'convo_state'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    discord_id = Column(Integer, nullable=False)
-    guild_id = Column(Integer, nullable=False)
-    conversation = Column(String, nullable=False)
-    state = Column(String, nullable=False)
-    email = Column(String)
+    discord_id = Column(UnsignedInt, nullable=False)
+    guild_id = Column(UnsignedInt, nullable=False)
+    conversation = Column(String(255), nullable=False)
+    state = Column(String(255), nullable=False)
+    email = Column(String(255))
 
     def __repr__(self):
         return f"<ConvoStep(guild_id={self.guild_id}, discord_id={self.discord_id}, "\
--- src/models/participant.py
+++ src/models/participant.py
@@ -1,13 +1,13 @@
 from sqlalchemy import Column, Integer, String
-from . import Base
+from . import Base, UnsignedInt
 
 
 class Participant(Base):
     __tablename__ = 'participants'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    discord_id = Column(Integer, nullable=False)
-    guild_id = Column(Integer, nullable=False)
+    discord_id = Column(UnsignedInt, nullable=False)
+    guild_id = Column(UnsignedInt, nullable=False)
     email = Column(String(255), nullable=False)
     institution = Column(String(255), nullable=False)
     role = Column(String(255), default='Participant')
